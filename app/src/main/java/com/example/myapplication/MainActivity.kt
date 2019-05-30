47028a0e4ece30c11e35ee46c835b5a3fa37c93d
densis <44565047+densis@users.noreply.github.com>
2019-05-30 12:20:41 +0900

    titile
    description

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// first
+// second
+// three
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
