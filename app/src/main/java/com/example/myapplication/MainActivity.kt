3a4139f8c76c760b91726679aaf035b883cda09e
densis <44565047+densis@users.noreply.github.com>
2019-05-30 12:30:44 +0900

    squash-title
    squash-description
    adfs 
    asf ads
     fads
     fas
    d dsa
     
    a f ads

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,9 +3,6 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-// first
-// second
-// three
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
